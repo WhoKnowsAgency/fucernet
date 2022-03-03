cce65c85f9c531970020f2f76ab7980f16d06e0d
Andy Damesón <andresdameson@gmail.com>
2022-03-03 11:22:18 -0300

    Agrega suplemento Marzo 22

--- pages/suplemento.vue
+++ pages/suplemento.vue
@@ -8,6 +8,18 @@
           <article class="normativa-module">
             <div class="main">
               <h4 ref="pageFocusTarget">
+                <a href="https://bit.ly/fucernet-suplemento-22" target="_blank">
+                  Suplemento Registral Nro. 22
+                </a>
+              </h4>
+            </div>
+            <div class="extra">
+              <small><a>Marzo 2022</a></small>
+            </div>
+          </article>
+          <article class="normativa-module">
+            <div class="main">
+              <h4 ref="pageFocusTarget">
                 <a href="https://bit.ly/fucernet-suplemento-21" target="_blank"
                   >Suplemento Registral Nro. 21</a
                 >
