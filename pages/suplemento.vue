a39196031db0128d209f009b712719fcf34f8779
Andy Damesón <andresdameson@gmail.com>
2022-05-02 12:35:53 -0300

    Agrega suplementos de abril/mayo

--- pages/suplemento.vue
+++ pages/suplemento.vue
@@ -8,6 +8,30 @@
           <article class="normativa-module">
             <div class="main">
               <h4 ref="pageFocusTarget">
+                <a href="https://bit.ly/fucernet-suplemento-24" target="_blank">
+                  Suplemento Registral Nro. 24
+                </a>
+              </h4>
+            </div>
+            <div class="extra">
+              <small><a>Mayo 2022</a></small>
+            </div>
+          </article>
+          <article class="normativa-module">
+            <div class="main">
+              <h4 ref="pageFocusTarget">
+                <a href="https://bit.ly/fucernet-suplemento-23" target="_blank">
+                  Suplemento Registral Nro. 23
+                </a>
+              </h4>
+            </div>
+            <div class="extra">
+              <small><a>Abril 2022</a></small>
+            </div>
+          </article>
+          <article class="normativa-module">
+            <div class="main">
+              <h4 ref="pageFocusTarget">
                 <a href="https://bit.ly/fucernet-suplemento-22" target="_blank">
                   Suplemento Registral Nro. 22
                 </a>
