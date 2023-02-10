4c453c4b02c1ef9d98af239ddba7788855bedd8e
Juan Germano <germi@hey.com>
2023-02-10 18:16:13 -0300

    suplemento

--- pages/suplemento.vue
+++ pages/suplemento.vue
@@ -8,6 +8,18 @@
           <article class="normativa-module">
             <div class="main">
               <h4 ref="pageFocusTarget">
+                <a href="https://bit.ly/fucernet-suplemento-33" target="_blank">
+                  Suplemento Registral Nro. 33
+                </a>
+              </h4>
+            </div>
+            <div class="extra">
+              <small><a>Febrero 2023</a></small>
+            </div>
+          </article>
+          <article class="normativa-module">
+            <div class="main">
+              <h4 ref="pageFocusTarget">
                 <a href="https://bit.ly/fucernet-suplemento-32" target="_blank">
                   Suplemento Registral Nro. 32
                 </a>
